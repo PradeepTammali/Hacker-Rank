bb198bea6e3cedcda6c5649b66c04eb40c148178
Pradeep Tammali <pradeepkumartammali@gmail.com>
2018-04-04 01:29:54 +0530

    Wed Apr  4 01:29:54 IST 2018

--- HourRank 27/MaximizingtheProfit.py
+++ HourRank 27/MaximizingtheProfit.py
@@ -1,1 +1,16 @@
-    
+#!/bin/python3
+
+import os
+import sys
+
+def maximumProfit(p):
+    print(p)
+    return 0
+
+if __name__ == '__main__':
+    fptr = open(os.environ['PWD']+'/output.txt', 'w')
+    n = int(input())
+    p = list(map(int, input().rstrip().split()))
+    result = maximumProfit(p)
+    fptr.write(str(result) + '\n')
+    fptr.close()
